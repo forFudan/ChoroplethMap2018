c30849c64d30f2254f77e6710a0deb9cd38a6695
Yuhao Zhu <y.zhu@ese.eur.nl>
2018-05-10 23:26:54 +0200

    change

--- main.py
+++ main.py
@@ -160,12 +160,14 @@ df_map_taiwan.set_index(
     [list(range(rows_china+1, rows_china+rows_taiwan+1))], inplace=True)
 
 
-#%%
+# %%
 """
-Combine the maps.
+Combine the two maps (mainland and Taiwan).
+This is not useful for other countries.
 """
 
 df_map = df_map_china.append(df_map_taiwan)
+
 
 """
 Import the data file.
@@ -174,25 +176,22 @@ Import the data file.
 df_price = pd.read_csv(path_data_file)
 df_map = pd.merge(df_map, df_price, how='left', on='region')
 
-
-df_map['density_m2'] = df_map['price(million)'] / \
-    df_map['region_area_m2'] / 100
-df_map['density_km2'] = df_map['price(million)'] / \
-    df_map['region_area_km2'] / 100
-df_map['density_100km2'] = df_map['price(million)'] / \
-    df_map['region_area_100km2'] / 100
+# Define density.
+df_map['density_m2'] = df_map['price(million)'] / df_map['region_area_m2'] / 100
+df_map['density_km2'] = df_map['price(million)'] / df_map['region_area_km2'] / 100
+df_map['density_100km2'] = df_map['price(million)'] / df_map['region_area_100km2'] / 100
 
 df_map.replace(to_replace={'density_m2': {0: np.nan}, 'density_km2': {
                0: np.nan}, 'density_100km2': {0: np.nan}}, inplace=True)
 
-#%%
+# %%
 """
 # Calculate Jenks natural breaks for density
 breaks = nb(
     df_map[df_map['density_100km2'].notnull()]['density_100km2'].values,
     initial=300,
     k=5)
-# the notnull method lets us match indices when joining
+
 jb = pd.DataFrame({'jenks_bins': breaks.yb}, index=df_map[df_map['density_100km2'].notnull()].index)
 df_map = df_map.join(jb)
 df_map.jenks_bins.fillna(-1, inplace=True)
@@ -203,6 +202,7 @@ jenks_labels = ["<= {:d} establishments per 100 km$^2$".format(int(b)) for b in 
 print(min(list(df_map['density_100km2'].values)))
 print(max(list(df_map['density_100km2'].values)))
 
+# Define manually the jenks
 
 breaks = [0., 8., 128., 2048., 16384., 262144.] + [1e20]
 df_map['jenks_bins'] = df_map['density_100km2'].apply(
@@ -211,7 +211,6 @@ jenks_labels = ["<= {:d} million RMB per 100 km$^2$".format(
     int(perc)) for perc in breaks[1:-1]]
 
 print(jenks_labels)
-
 
 print(min(list(df_map['jenks_bins'].values)))
 print(max(list(df_map['jenks_bins'].values)))
@@ -225,21 +224,22 @@ plt.clf()
 fig = plt.figure()
 ax = fig.add_subplot(111, frame_on=False)
 
-# use a blue colour ramp - we'll be converting it to a map using cmap()
+"""
+Transfer the color to blue.
+"""
 # http://matplotlib.org/examples/color/colormaps_reference.html
 cmap = plt.get_cmap('Blues')
 
-# draw wards with grey outlines
-df_map['patches'] = df_map['poly'].map(lambda x: PolygonPatch(
-    x, ec='#555555', lw=.2, alpha=0.75, zorder=4))
+# Draw the regions using gray bolders.
+df_map['patches'] = df_map['poly'].map(lambda x: PolygonPatch(x, ec='#555555', lw=.2, alpha=0.75, zorder=4))
 pc = PatchCollection(df_map['patches'], match_original=True)
 
-# impose our colour map onto the patch collection
+# Put the map on the patches.
 norm = Normalize()
 pc.set_facecolor(cmap(norm(df_map['jenks_bins'].values)))
 ax.add_collection(pc)
 
-# Add a colour bar
+# Put the color bar onto the map.
 cb = colorbar_index(ncolors=len(jenks_labels), cmap=cmap,
                     shrink=0.3, labels=jenks_labels)
 cb.ax.tick_params(labelsize=7)
@@ -257,7 +257,7 @@ cb.ax.tick_params(labelsize=7)
 #     size=5,
 #     color='#555555')
 
-# Bin method, copyright and source data info
+# The bin methods and other information.
 smallprint = ax.text(
     1, 0.1,
     'Data from: www.landchina.com\nData for Taiwan not available\nPlotted by: Yuhao Zhu\nCopyright at 2017',
@@ -265,7 +265,6 @@ smallprint = ax.text(
     size=7,
     color='#555555',
     transform=ax.transAxes)
-
 
 map_taiwan.scatter(
     [geom.x for geom in []],
